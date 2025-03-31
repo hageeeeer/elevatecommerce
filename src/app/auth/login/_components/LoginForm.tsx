89935a64c42833ea2b0fe7508ecb857b01278854
HajarsaMer <hagersamer21@gmail.com>
2025-03-31 18:07:07 +0200

    update login file

--- src/app/auth/login/_components/LoginForm.tsx
+++ src/app/auth/login/_components/LoginForm.tsx
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 import { InfinitySpin } from 'react-loader-spinner'
 import React, { useState } from 'react'
-import { useRouter } from 'next/navigation';
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
 import { signIn } from 'next-auth/react';
