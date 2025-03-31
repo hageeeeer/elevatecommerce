632b3bb31173b6f96ee2ba663321b16e86c103d6
HajarsaMer <hagersamer21@gmail.com>
2025-03-31 18:51:03 +0200

    umiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,30 +19,28 @@
 //   matcher: ['/cart','/dashboard'],
 // }
 
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-const privatePages = new Set(['/cart', '/dashboard']); // Add your protected routes here
+const privatePages = new Set(['/cart', '/dashboard']); // Protected routes
 
 export default async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  
-  // Check if the current page is a private one (like /cart or /dashboard)
   if (privatePages.has(req.nextUrl.pathname)) {
     if (token) {
-      return NextResponse.next(); // Proceed if the user is authenticated
+      console.log('token',token);
+      
+      return NextResponse.next(); // User is authenticated, continue to the protected page
     }
 
-    const callbackUrl = req.nextUrl.pathname; // Store the current URL (e.g., /cart)
-    const redirectUrl = new URL('/auth/login', req.nextUrl.origin); // Redirect to login page
-    redirectUrl.searchParams.set('callbackUrl', callbackUrl); // Add the callbackUrl as query parameter
-
-    return NextResponse.redirect(redirectUrl); // Redirect the user to the login page
+    // User is not authenticated, redirect to login page
+    const loginUrl = new URL('/auth/login', req.nextUrl.origin);
+    return NextResponse.redirect(loginUrl);
   }
 
-  return NextResponse.next();
+  return NextResponse.next(); // Continue normally for non-protected routes
 }
 
 export const config = {
-  matcher: ['/cart', '/dashboard'], // Match protected routes
+  matcher: ['/cart', '/dashboard'], // Define the routes to be protected
 };
