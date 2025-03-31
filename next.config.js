5649f64f894d0a4844334bec96643c0580f0edb6
HajarsaMer <hagersamer21@gmail.com>
2025-03-31 19:21:40 +0200

    unextconfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 // next.config.js
 module.exports = {
+  experimental: {
+    middleware: true, // Enable edge middleware
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://elevatecommerce.netlify.app',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/cart',
+        destination: '/auth/login', // Redirect to login page if not authenticated
+        permanent: false,
+      },
+    ];
+  },
     images: {
       domains: ['flower.elevateegy.com'],  // Ensure this is added
       remotePatterns: [
